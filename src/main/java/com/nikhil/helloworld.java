41352439fa0d7971175d0506d47666e8afcb2237
Nikhil <nikhilankola2@gmail.com>
2026-01-14 09:53:15 +0530

    Add Java sources with correct package structure

--- src/main/java/com/nikhil/helloworld.java
+++ src/main/java/com/nikhil/helloworld.java
@@ -2,6 +2,7 @@ package com.nikhil;
 
 public class helloworld { 
 	public static void main(String[] args){ 
-		System.out.println("Hello,World"); 
+		System.out.println("Hello,World");
+		System.out.println("Bye bye"); 
 	} 
 }
